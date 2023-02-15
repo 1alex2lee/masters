0e2996db88e35a5294f240b6ebd2b58f6c82ff85
1alex2lee <lal19@ic.ac.uk>
2023-02-15 22:30:59 +0000

    fixed import die

--- python/die.py
+++ python/die.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+import os, threading, concurrent.futures
 import re #for splitting strings
 import meshio
 from scipy.interpolate import griddata
@@ -11,6 +11,10 @@ import main
 dieImagesPath = ""
 punchImagesPath = ""
 Edges_dir = ""
+imageResolution_H = 384
+imageResolution_W = 256
+input = np.ones((5,imageResolution_W, imageResolution_H))
+blank_loaded = False
 
 ### HELPER FUNCTIONS ###
 
@@ -76,39 +80,48 @@ def is_in_poly(p, poly):
                 is_in = not is_in
     return is_in
 
-def postprocess(i, file, edgefile, baseLength_W, baseLength_H, imageResolution_W, imageResolution_H, zoom:bool):
-    print("-> DIE START", file, "\n", "zoom ", zoom)
 
-    # load the mesh
-    fileName = dieImagesPath + file
-    mesh = meshio.read(filename=fileName)
+def postprocess(i, file, edgefile, imageResolution_W, imageResolution_H, qml):
+    global input
 
-    # offset and flip
-    mesh.points[:, 2] = mesh.points[:, 2] - mesh.points[:, 2].max()  # offset ensures z=0 base
-    mesh.points[:, 2] = -1 * mesh.points[:, 2]
+    zoom = i == 1
+    blank = i == 4
+
+    print("-> IMAGE START ", "channel:", i, "zoom:", zoom, " blank:", blank)
 
     # interpolate to grid
     if zoom:
+        baseLength_H = 1090  # in mm
+        baseLength_W = -660  # in mm
         X = np.linspace(790, baseLength_H, imageResolution_H)
-        Y = np.linspace(-460, baseLength_W, imageResolution_W)
+        Y = np.linspace(-460, baseLength_W, imageResolution_W,)
     else:
+        baseLength_H = 1200  # in mm
+        baseLength_W = -800  # in mm
         X = np.linspace(0, baseLength_H, imageResolution_H)
         Y = np.linspace(0, baseLength_W, imageResolution_W)
+
     gridX, gridY = np.meshgrid(X, Y)
-    dieImage = np.ones((imageResolution_W, imageResolution_H))
+    image = np.ones((imageResolution_W, imageResolution_H))
 
-    interpolatedImage = griddata(mesh.points[:, 0:2], mesh.points[:, 2], (gridX, gridY), method='linear', fill_value=0)
-    dieImage[:, :] = interpolatedImage
+    if not blank:
+        # load the mesh
+        fileName = dieImagesPath + file
+        mesh = meshio.read(filename=fileName)
 
-    meshFilePath = Edges_dir + edgefile
+        interpolatedImage = griddata(mesh.points[:, 0:2], mesh.points[:, 2], (gridX, gridY), method='linear', fill_value=0)
+        image[:, :] = interpolatedImage
+
+        # offset and flip
+        mesh.points[:, 2] = mesh.points[:, 2] - mesh.points[:, 2].max()  # offset ensures z=0 base
+        mesh.points[:, 2] = -1 * mesh.points[:, 2]
 
     nodalIDs = []
     nodalCoordinates = []
     elementIDs = []
     elementConnectivity = []
-    nodeConnectivity_elementIndicies = []
 
-    with open(meshFilePath) as f:  # open file j
+    with open(edgefile) as f:  # open file j
 
         # Iterate through lines
         for line in f.readlines():
@@ -130,7 +143,7 @@ def postprocess(i, file, edgefile, baseLength_W, baseLength_H, imageResolution_W
                 temp = np.int_(temp)  # convert the strings into ints
                 elementConnectivity.append(temp)  # store connectivity in a list
 
-    nodalCoordinates = meshio.read(meshFilePath).points
+    nodalCoordinates = meshio.read(edgefile).points
 
     # convert into np arrays
     nodalIDs = np.array(nodalIDs)
@@ -143,50 +156,91 @@ def postprocess(i, file, edgefile, baseLength_W, baseLength_H, imageResolution_W
         ID = np.searchsorted(nodalIDs, elementConnectivity[e])
         poly.append([[nodalCoordinates[ID[0], 0], nodalCoordinates[ID[0], 1]],
                      [nodalCoordinates[ID[1], 0], nodalCoordinates[ID[1], 1]]])
+        
+    length = len(X)*len(Y)
+    idx = 0
 
     for p in range(len(X)):
         for q in range(len(Y)):
             if not is_in_poly([X[p], Y[q]], poly):
-                dieImage[q, p] = 0
+                image[q, p] = 0
 
-    print("-> DIE DONE", file, "\n")
+            if zoom:
+                qml.load_pred_mesh_progress.emit("zoom", idx/length)
+            elif blank:
+                qml.load_pred_mesh_progress.emit("blank", idx/length)
+            else:
+                qml.load_pred_mesh_progress.emit("non zoom", idx/length)
 
-    return dieImage
+            idx += 1
+
+    print("-> IMAGE DONE", "channel:", i, "zoom:", zoom, " blank:", blank)
+
+    if not blank:
+        image = -image
+
+    input[i, :, :] = image
+    plt.imsave(os.path.join("temp",f"input_{i}.png"), image)
+
+    if zoom:
+        qml.load_pred_mesh_complete.emit("zoom")
+    elif blank:
+        qml.load_pred_mesh_complete.emit("blank")
+    else:
+        qml.load_pred_mesh_complete.emit("non zoom")
+    
+    np.save(os.path.join("temp","input.npy"), input)
 
 
-def load(die_dir, edge_dir):
+def load(die_dir, edge_dir, blank_dir, qml):
+    global blank_loaded
 
     # sortedFiles = sortFiles(dieImagesPath, '.nas')
     # edgeFiles = sortedgeFiles(Edges_dir, 'Die.nas')
 
-    sortedFiles = die_dir[7:]
-    edgeFiles = edge_dir[7:]
+    if blank_dir == "":
+        dieFile = die_dir[7:]
+        edgeFile = edge_dir[7:]
 
-    print("die dir: ",sortedFiles)
-    print("edge dir: ",edgeFiles)
+        print("die dir: ", dieFile)
+        print("edge dir: ", edgeFile)
 
-    # fixed parameters
-    # imageResolution_H = [384 for i in range(len(sortedFiles))]
-    # imageResolution_W = [256 for i in range(len(sortedFiles))]
-    # baseLength_H = [1090 for i in range(len(sortedFiles))]  # in mm
-    # baseLength_W = [-660 for i in range(len(sortedFiles))]  # in mm
+        input_range = [0,1]  # die, die zoomed
+        edgeFiles = [edgeFile, edgeFile]
+    else:
+        dieFile = die_dir[7:]
+        edgeFile = edge_dir[7:]
+        blankFile = blank_dir[7:]
+
+        print("die dir: ", dieFile)
+        print("edge dir: ", edgeFile)
+        print("blank dir: ", blankFile)
+
+        input_range = [0,1,4]  # die, die zoomed, blank
+        edgeFiles = [edgeFile, edgeFile, blankFile]
+
+        blank_loaded = True
 
     imageResolution_H = 384
     imageResolution_W = 256
-    # baseLength_H = 1090  # in mm
-    # baseLength_W = -660  # in mm
 
-    # obtain die images
-    # dieImages = np.zeros((imageResolution_W, imageResolution_H))
+    # input = np.zeros((5,imageResolution_W, imageResolution_H))
 
     # with concurrent.futures.ProcessPoolExecutor(max_workers = 10) as executor:
-    #     for i, dieImage in executor.map(postprocess, 1, sortedFiles, edgeFiles, baseLength_W, baseLength_H, imageResolution_W, imageResolution_H):
-    #         dieImages[i, :, :] = dieImage
+    #     for idx, input_i in enumerate(input_range):
+    #         dieImage = postprocess (input_i, dieFile, edgeFiles[idx], baseLength_W, baseLength_H, imageResolution_W, imageResolution_H, qml)
+    #         input[input_i, :, :] = dieImage
 
-    dieImage = postprocess (0, sortedFiles, edgeFiles, -660, 1090, imageResolution_W, imageResolution_H, zoom=False)
-    dieImage_zoom = postprocess (0, sortedFiles, edgeFiles, -660, 1090, imageResolution_W, imageResolution_H, zoom=True)
+    # np.save(os.path.join("temp","input.npy"), input)
 
-    np.save(os.path.join("temp","input_1.npy"), dieImage)
-    np.save(os.path.join("temp","input_2.npy"), dieImage_zoom)
+    # for i in input_range:
+    #     plt.imsave(os.path.join("temp",f"input_{i}.png"), input[i,:,:])
 
-    plt.imsave(os.path.join("temp","input_1.png"), dieImage)
+    for idx, input_i in enumerate(input_range):
+        threading.Thread(target=postprocess, args=(input_i, dieFile, edgeFiles[idx], imageResolution_W, imageResolution_H, qml)).start()
+
+
+def is_blank_loaded ():
+    global blank_loaded
+
+    return blank_loaded
